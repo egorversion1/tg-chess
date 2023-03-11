9aa5ceaba45e93ce174b6bf76ba5c927667dfcef
egorversion1 <35696992+egorversion1@users.noreply.github.com>
2023-03-10 23:26:29 -0800

    upload files

---
+++ chess-server/server.js
@@ -0,0 +1,13 @@
+const logger = require('morgan');
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
